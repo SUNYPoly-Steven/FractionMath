317d523a040222689b509ad214bd4bfb93b8bdb2
TryOSdev-Steven <steven35953@gmail.com>
2016-04-12 18:17:19 -0400

    Update FractionMath.cpp
    made the code complieable. (Warning, may cause infinite loops)

--- FractionMath.cpp
+++ FractionMath.cpp
@@ -12,8 +12,17 @@
 #define FINAL    8
 
 int alpha_index(const char* c); //returns the index of the char in the alpha array (returns -1 if fails)
+const char* one(const char*);
+const char* two(const char*);
+const char* four(const char*);
+const char* five(const char*);
+const char* six(const char*);
+const char* seven(const char*);
+const char* eight(const char*);
+const char* nine(const char*);
 
-char* (*acts[STATES])(const char*) = {NULL, one, two, NULL, four, five, six, seven, eight, nine};
+
+const char* (*acts[STATES])(const char*) = {NULL, one, two, NULL, four, five, six, seven, eight, nine};
 char alpha[ALPHA] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*', '\n', '\0' };
 int trans[STATES][ALPHA] = {
 	         /*0   1   2   3   4   5   6   7   8   9   +   -   /   *   s  \n  \0*/
@@ -28,6 +37,24 @@ int trans[STATES][ALPHA] = {
 	/* 8*/  { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
 	/* 9*/  { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  4,  4,  4,  4, -1, -1, -1 },
 };
+
+struct frac {
+	int whole;
+	int numerator;
+	int denominator;
+	bool sign; //+ is true
+	
+	frac() {
+		whole = 0;
+		numerator = 0;
+		denominator = 0;
+		sign = true;
+	}
+	
+};
+
+frac a;
+frac b;
 
 
 int main(int argc, char* argv[]) {
@@ -63,38 +90,47 @@ int main(int argc, char* argv[]) {
 }
 
 int alpha_index(const char* c) {
-	
+	return -1;
 }
 
-char* one(const char* c) {
-	std::cout << "one" << std::endl;
+const char* one(const char* c) {
+	//this should be a [+/-]
+	a.sign = (*c == '+') ? true : false;
+	return c;//what do I return?
 }
 
-char* two(const char* c) {
+const char* two(const char* c) {
 	std::cout << "two" << std::endl;
+	return c;
 }
 
-char* four(const char* c) {
+const char* four(const char* c) {
 	std::cout << "four" << std::endl;
+	return c;
 }
 
-char* five(const char* c) {
+const char* five(const char* c) {
 	std::cout << "five" << std::endl;
+	return c;
 }
 
-char* six(const char* c) {
+const char* six(const char* c) {
 	std::cout << "six" << std::endl;
+	return c;
 }
 
-char* seven(const char* c) {
+const char* seven(const char* c) {
 	std::cout << "seven" << std::endl;
+	return c;
 }
 
-char* eight(const char* c) {
+const char* eight(const char* c) {
 	std::cout << "eight" << std::endl;
+	return c;
 }
 
-char* nine(const char* c) {
+const char* nine(const char* c) {
 	std::cout << "nine" << std::endl;
+	return c;
 }
 
